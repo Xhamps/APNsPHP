6f218404c24ae526395322cdf2f5fabeba6e474f
Xhamps <xhamps@gmail.com>
2013-06-23 01:29:40 -0300

    Added Database

---
+++ src/sql/apn.sql
@@ -0,0 +1,15 @@
+--
+-- Table structure for table `apn_devices`
+--
+
+CREATE TABLE `apn_devices` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `devicetoken` char(64) NOT NULL,
+  `devicename` varchar(255) NOT NULL,
+  `devicemodel` varchar(100) NOT NULL,
+  `deviceversion` varchar(25) NOT NULL,
+  `status` enum('active','uninstalled') NOT NULL DEFAULT 'active',
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Store devices' AUTO_INCREMENT=1 ;
