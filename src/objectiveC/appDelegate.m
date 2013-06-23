7df65bc3eabb57c5e9b0cdc6f8f87f338e770283
Xhamps <xhamps@gmail.com>
2013-06-23 02:58:56 -0300

    Code refactoring in code of device

--- src/objectiveC/appDelegate.m
+++ src/objectiveC/appDelegate.m
@@ -9,35 +9,41 @@
   application.applicationIconBadgeNumber = 0;
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [self sendProviderDeviceToken:deviceToken];
 
-  // Get the users Display Name, Device Model, Token & Version Number
-  UIDevice *dev = [UIDevice currentDevice];
-  NSString *deviceName = dev.name;
-  NSString *deviceModel = dev.model;
-  NSString *deviceSystemVersion = dev.systemVersion;
-
-  NSString *deviceToken = [[[[devToken description]
-    stringByReplacingOccurrencesOfString:@"<"withString:@""]
-    stringByReplacingOccurrencesOfString:@">" withString:@""]
-    stringByReplacingOccurrencesOfString: @" " withString: @""];
-
-  // URL of Service
-  // CHANGE TO THE PATH OF SERVICE
-  NSString *urlHost = @"www.mywebsite.com/register.model.php";
-
-  NSString *urlString = [NSString stringWithFormat:@"?devicetoken=%@&devicename=%@&devicemodel=%@&deviceversion=%@", deviceToken, deviceName, deviceModel, deviceSystemVersion;
-
-
-  NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-  NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-  NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-
+  NSLog(@"Did register for remote notifications: %@", deviceToken);
 
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
 
     NSLog(@"Fail to register for remote notifications: %@", error);
+
+}
+
+- (BOOL)sendProviderDeviceToken:(NSString *)devToken {
+  UIDevice *dev = [UIDevice currentDevice];
+  NSString *deviceName = dev.name;
+  NSString *deviceModel = dev.model;
+  NSString *deviceSystemVersion = dev.systemVersion;
+
+  NSString *deviceToken = [[[[devToken description]
+                             stringByReplacingOccurrencesOfString:@"<"withString:@""]
+                            stringByReplacingOccurrencesOfString:@">" withString:@""]
+                           stringByReplacingOccurrencesOfString: @" " withString: @""];
+
+  // URL of Service
+  // CHANGE TO THE PATH OF SERVICE
+  NSString *urlHost = @"www.mywebsite.com/register.model.php";
+
+  NSString *urlString = [NSString stringWithFormat:@"?devicetoken=%@&devicename=%@&devicemodel=%@&deviceversion=%@", deviceToken, deviceName, deviceModel,deviceSystemVersion];
+
+
+  NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:urlHost path:[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+  NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+  NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+
+  return YES;
 
 }
